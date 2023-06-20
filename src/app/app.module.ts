1db68e19e4cbf27894d7930a4ed3676f69991d71
fatmaa <fatma.asik@matriksdata.com>
2023-06-20 17:31:15 +0300

    test netlify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { WordListModule } from './word-list/module';
 import { HttpService } from './shared/services/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -47,10 +48,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     NbContextMenuModule,
     NbEvaIconsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      enabled: environment.production,
     }),
   ],
   providers: [HttpService, NbSidebarService, WordListService, NbWindowService],
